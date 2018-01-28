93decdf264f7dfdd57181e6ff242708052a0ec44
mhe12 <hemingxi@hemingxideMacBook-Pro.local>
2018-01-27 18:40:25 -0800

    Java servlets

---
+++ rpc/SearchItem.java
@@ -0,0 +1,74 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import db.mysql.MySQLConnection;
+import entity.Item;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet("/search")
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private MySQLConnection conn = MySQLConnection.getInstance();
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SearchItem() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	/*
+	 * HttpServletRequest request 打包好了输入 HttpServletResponse response 打包好了输出
+	 * 可以往里面加东西，response是发给前端浏览器的object
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {// doGet是interface名，改了就不认识了
+		String userId = request.getParameter("user_id");
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		// Term can be empty or null.
+		String term = request.getParameter("term");
+		List<Item> items = conn.searchItems(userId, lat, lon, term);
+		List<JSONObject> list = new ArrayList<>();
+		try {
+			for (Item item : items) {
+				JSONObject obj = item.toJSONObject();
+				list.add(obj);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray array = new JSONArray(list);
+		RpcHelper.writeJsonArray(response, array);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
